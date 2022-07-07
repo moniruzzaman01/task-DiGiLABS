c9a445c09c3c5f488a0848ef6e865ddc1fc31a77
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2022-07-07 12:21:10 +0600

    hero section done

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./Hero.css";
 import Navbar from "./Navbar";
+import img1 from "../assets/Ellipse 1.png";
+import img2 from "../assets/Ellipse 2.png";
+import img3 from "../assets/Ellipse 3.png";
+import img4 from "../assets/Ellipse 4.png";
 
 const Hero = () => {
   return (
     <div className=" hero h-[704px] ">
       <Navbar />
-      <div className=" flex h-full mt-32 px-16 ">
-        <div className=" flex-1">
+      <div className=" flex h-full pl-16 ">
+        <div className=" flex-1 mt-32">
           <div>
             <p className=" text-[14px] text-[#9921eb] uppercase bg-white px-3 rounded-full inline-block ">
               #MOST TRUSTED CERTIFICATIONS BY THE INDUSTRY
@@ -40,7 +44,20 @@ const Hero = () => {
             </div>
           </div>
         </div>
-        <div className=" flex-1">{/* image section */}</div>
+        <div className=" flex flex-1 relative">
+          <div className=" absolute bottom-28">
+            <img className=" h-[88px] ml-56" src={img4} alt="" />
+            <img className=" h-[128px] my-10 ml-20" src={img3} alt="" />
+            <img className=" h-[228px] ml-[-45px]" src={img2} alt="" />
+          </div>
+          <div>
+            <img
+              className=" h-[586px] absolute bottom-0 right-0"
+              src={img1}
+              alt=""
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
