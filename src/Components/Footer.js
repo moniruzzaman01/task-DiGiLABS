b5a55b6666efa60f36aaf547a56f2f316188fc74
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2022-07-07 18:17:56 +0600

    footer added

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,16 @@
 import React from "react";
 import call from "../assets/call.png";
 import message from "../assets/message.png";
+import facebook from "../assets/facebook.png";
+import youtube from "../assets/youtube.png";
+import browser from "../assets/browser.png";
+import figma from "../assets/figma.png";
+import whatsApp from "../assets/whats-app.png";
 
 const Footer = () => {
   return (
     <div className=" bg-[#27282b] pt-12 pb-10 mt-[150px] ">
-      <div className=" flex px-[150px] gap-[100px] ">
+      <div className=" flex px-[150px] gap-[100px] mb-20 ">
         <div className=" flex-1">
           <h1 className=" text-[24px] font-bold text-white  ">Logo</h1>
           <p className=" text-[14px] text-[#8c8c8c] mt-3 mb-6 ">
@@ -44,6 +49,30 @@ const Footer = () => {
             Submit
           </button>
         </div>
+      </div>
+      <div className=" text-[#8c8c8c] text-[14px] flex justify-between  px-[150px]">
+        <span>About us</span>
+        <span>Jobs</span>
+        <span>Blog</span>
+        <span>Press</span>
+        <span>FAQ</span>
+        <span>Careers</span>
+        <span>Contact</span>
+        <span>Privacy policy</span>
+        <span>Sitemap</span>
+        <span>Terms of Use</span>
+      </div>
+      <div className=" flex justify-between items-center px-[150px] mt-12 ">
+        <div className=" flex gap-3">
+          <img src={facebook} alt="" />
+          <img src={youtube} alt="" />
+          <img src={browser} alt="" />
+          <img src={figma} alt="" />
+          <img src={whatsApp} alt="" />
+        </div>
+        <p className=" text-[14px] text-[#8c8c8c] ">
+          © 2000-2021, All Rights Reserved
+        </p>
       </div>
     </div>
   );
